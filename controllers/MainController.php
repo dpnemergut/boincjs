b1f23d8477978f692b2b6901454e3a37249660a9
Daniel Nemergut <dpnemergut@gmail.com>
2014-11-21 04:10:24 -0500

    Created base controllers and sprinkled in some functionality

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -4,10 +4,31 @@
  *  Author: Daniel Nemergut
  *  File: controllers/MainController.php
  *
- *  Comment
+ *  This controller handles all requests coming in.
  */
 
-class MainController extends AbstractController {
+class MainController {
+
+    protected $model;
+
+    public function start($requestType, $stagingData = null) {
+        //$var = json_encode(array('test' => 'data'));
+        //return $var;
+        // Determine which model to load
+        switch ($requestType) {
+	    case 'init':
+                require_once('models/InitModel.php');
+                $this->model = new InitModel();
+                $newData = $this->model->fetchNewData();
+	        break;
+	    case 'staging':
+                if($stagingData !== null) {
+                    require_once('models/StagingModel.php');
+                    $this->model = new StagingModel();
+                    $this->model->enterData($stagingData);
+                }
+                break;
+        }
+    }
 
 }
-
---
+++ controllers/ReviewController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Project: boincjs
+ *  Author: Daniel Nemergut
+ *  File: controllers/ReviewController.php
+ *
+ *  This controller handles the logic of whether or not to accept a set of staging data.
+ */
+
+class ReviewController {
+
+    protected $stagingModel;
+    protected $finalModel;
+
+    /**
+     *  Determines whether the calculated data should be accepted or rejected.
+     *  It then loads the appropriate model to store the data.
+     */
+    public function review($rawId) {
+        // Load all the calculated data from the staging table
+        $this->stagingModel = new StagingModel();
+        $stagingData = $this->stagingModel->loadAllStagingDataForId($rawId);
+
+        // Get an average of the results
+    }
+
+}
