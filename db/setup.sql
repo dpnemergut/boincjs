23bfd21db491d8253bdde7b9c709f60b4f33b902
Daniel Nemergut <dpnemergut@gmail.com>
2014-11-21 04:10:41 -0500

    Basic DB install script

--- db/setup.sql
+++ db/setup.sql
@@ -11,15 +11,22 @@ CREATE SCHEMA IF NOT EXISTS boincjs_db;
 USE boincjs_db;
 SELECT '<Starting DB startup kit...>' AS ' ';
 
+DROP TABLE IF EXISTS rejected;
+DROP TABLE IF EXISTS accepted;
+DROP TABLE IF EXISTS staging;
+DROP TABLE IF EXISTS raw;
 SELECT '<Creating table raw...>' AS ' ';
 CREATE TABLE raw (
-    raw_id int NOT NULL AUTO_INCREMENT PRIMARY KEY
+    raw_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    distributed_count int NOT NULL DEFAULT 0,
+    raw_data text NOT NULL DEFAULT ''
 );
 
 SELECT '<Creating table staging...>' AS ' ';
 CREATE TABLE staging (
     staging_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     raw_id int NOT NULL,
+    results text NOT NULL DEFAULT '',
     FOREIGN KEY (raw_id) REFERENCES raw(raw_id)
 );
 
@@ -36,3 +43,9 @@ CREATE TABLE rejected (
     raw_id int NOT NULL,
     FOREIGN KEY (raw_id) REFERENCES raw(raw_id)
 );
+
+SELECT '<Inserting raw data...>' AS ' ';
+INSERT INTO raw (raw_data) VALUES
+    ('{10}'),
+    ('{20}'),
+    ('{30}');
